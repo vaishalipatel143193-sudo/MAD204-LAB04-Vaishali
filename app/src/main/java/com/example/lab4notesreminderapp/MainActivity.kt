b36c7ba79206f9ce74c8bb8f15462a266effbd43
vaishalipatel143193-sudo <vaishalipatel143193@gmail.com>
2025-12-11 11:03:07 -0800

    New Release

--- app/src/main/java/com/example/lab4notesreminderapp/MainActivity.kt
+++ app/src/main/java/com/example/lab4notesreminderapp/MainActivity.kt
@@ -23,7 +23,6 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    //Main
     private lateinit var db: NotesDatabase
     private lateinit var adapter: MyAdapter
     private val notes = mutableListOf<Note>()
